ef7fde94cfbe5610d66a8fffa74b2d9be3463460
Phoenissa <46357130+Phoenissa@users.noreply.github.com>
2019-01-25 11:50:24 +0100

    Update Frage.java

--- src/Quiz_Packages/File/Frage.java
+++ src/Quiz_Packages/File/Frage.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 
 public class Frage {
-    private String question;
+	//Initialisierung der Objekte der Klasse Frage: Frage, Antworten und korrekte Antworten werden als Strings (privat) erstellt; Chancen und verwendete Chancen als Integer; Wahrheitswert, ob Frage benutzt wurde
+    private String question;				
     private String answer1;
     private String answer2;
     private String answer3;
@@ -16,6 +17,7 @@ public class Frage {
 
 
     /**
+	 * Konstruktor der Klasse Frage: 
      * @param question
      * @param answer1
      * @param answer2
@@ -26,7 +28,7 @@ public class Frage {
      */
     public Frage(String question, String answer1, String answer2, String answer3, 
                                         String answer4, String correctAnswers, int chances){
-        //Teste, ob die Elemente wirklich existieren, d.h. ob dort nutzbare Informationen drinnen sind
+        //Teste, ob die Elemente wirklich existieren, d.h. ob dort nutzbare Informationen drinnen sind -> Fehlervermeidung durch Exception
         if (question == null || answer1 == null || answer2 == null|| answer3 == null || answer4 == null) throw new NullPointerException();
         if (question.equals("")||
                 answer1.equals("")||
@@ -39,75 +41,92 @@ public class Frage {
             throw new IllegalArgumentException();
         }                               
         
-        this.correctAnswers = correctAnswers;                       //bringe die KonstruktorVariablen in das Objekt
+		//Zuordnen der übergebenen Parameter zu den Objekten der Klasse Frage: this.question meint Objekt während question der Parameter ist
+        this.correctAnswers = correctAnswers;                 //bringe die KonstruktorVariablen in das Objekt
         this.question = question;
         this.answer1 = answer1;
         this.answer2 = answer2;
         this.answer3 = answer3;
         this.answer4 = answer4;
         this.chances = chances;
-        this.usedChances = chances-1;                                 //kopiere die Chancen, zweite kann bearbeitet werden
+        this.usedChances = chances-1;                         //kopiere die Chancen, zweite kann bearbeitet werden
     }
 
 
     /**
+	 * Getter-Methode für Anzahl der Chancen
      * Integer, amount of chances
      * @return
      */
-    public int getChances(){return chances;}
+    public int getChances(){
+		return chances;
+	}
 
     /**
+	 * Getter-Methode für die Anzahld er verwendeten Chancen
      * Integer, amount of used chances
      * @return
      */
-    public int getUsedChances(){return usedChances;}
+    public int getUsedChances(){
+		return usedChances;
+	}
 
     /**
+	 * Setter-Methode um verwendete Chancen um 1 zu verringern (dekrementieren)
      * decreases the amount of chances
      */
-    public void decChances(){                                               //decrementiere die Chancen, wenn Frage übersprungen wurde
+    public void decChances(){                                //decrementiere die Chancen, wenn Frage übersprungen wurde
         if (getChances() >1)
-              usedChances-=1;  
+              usedChances-=1;  //statt "usedChances-=1" evtl. "usedChances=usedChances--"?
     }
 
     /**
+	 * Getter-Methode um verbleibende Chancen zu errechnen und zurückzugeben
      * remaining chances
-     * @return
+     * @return ? (wenn: "int actC=chances-usedChances; return actC" wäre return actC, wie formuliert man das hier im JavaDoc?)
      */
-    public int getActChances(){                                             //gibt die Anzahl der Versuche zurück
+    public int getActChances(){                              //gibt die Anzahl der verbleibenden Versuche zurück
         return chances - usedChances;
     }
 
     /**
-     * a Question should'nt be used two times
+	 * Setter-Methode
+     * Vermeiden von Fragendopplungen (keine Frage soll 2mal benutzt werden)
      * @param b
      */
     public void setUsed(boolean b){
-        used = b;
+        used = b;								//übergebener Parameter wird als boolean in used gespeichert (wahr oder falsch gesetzt)
     }
 
     /**
-     *
-     * @return
+     * Getter-Methode um "used" zurückzugeben
+     * @return used
      */
     public boolean getUsed(){
         return used;
     }
 
     /**
+	 * Getter-Methode um Frage zurückzugeben
      * returns the question
-     * @return
+     * @return question
      */
-    public String getQuestion(){return this.question;}
+    public String getQuestion(){
+		return this.question;
+	}
 
     /**
+	 * Getter-Methode zur Rückgabe der richtigen Antwort als String
      * returns the correct answer
      * @return
      */
-    public String getCorrectAnswers(){return this.correctAnswers;}
+    public String getCorrectAnswers(){
+		return this.correctAnswers;
+	}
 
 
     /**
+     * Erstellt eine Liste(?) aus den Objekten der Klasse und gibt sie als solche zurück
      * simply return all possible answers
      * @return
      */
@@ -123,7 +142,12 @@ public class Frage {
         return kompletteFrage;
 
     }
-
+	
+	/**
+	 * Setter-Methode um Anzahl der Chancen zu verändern
+	 * @param chances (int)
+	 * @return chances
+	 */
     public void setChances(int chances){
         this.chances= chances;
     }
